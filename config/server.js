91cbe555a59e14f4b8996836b204c50117bef12e
Ruben Kretek <rubenkretek@outlook.com>
2021-02-04 19:29:21 +0000

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f8902aaa9e1ee956a6677b3ec50f1447'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e11edd2e-777d-4fd3-8d6b-32fff0f4f537'
+};
